6f624b50ad9220e00444d83678654dd2f795422f
koechkevin <koechkevin92@gmail.com>
2019-09-07 11:39:18 +0300

    first commit

--- api/database/firebase.js
+++ api/database/firebase.js
@@ -1,11 +1,10 @@
 import * as admin from 'firebase-admin';
 import env from 'dotenv';
-import path from 'path';
 
 env.config();
 
 admin.initializeApp({
-  credential: admin.credential.cert(path.resolve(__dirname, '..', '..', 'credentials.json')),
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CONFIG)),
   databaseURL: 'https://backend-fff45.firebaseio.com/'
 });
 const database = admin.database();
